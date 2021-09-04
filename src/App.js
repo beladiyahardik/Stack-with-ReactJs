eb2b377ab0bd2d1cc62284ad9b921253a003ec41
Hardik <beladiyahardik755@gmail.com>
2021-09-04 16:40:20 +0530

    Stack With ReactJs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react';
 function App() {
+  //Hook
+  const [newTitle, setnewTitle] = useState("");
+  const [isEdit , setIsEdit] = useState();
+  const [stack, setStack] = useState([]);
+
+  useEffect(() => {
+   console.log(stack);
+  }, [stack])
+
+  useEffect(() => {
+    const notice = document.querySelector('.notice');
+    notice.style.visibility ="hidden";
+  })
+
+  const handleChnageData = () => {
+
+    if(newTitle){
+      if([...stack].length != 5){
+        setStack([...stack, newTitle]);
+        setnewTitle('');
+      }else{
+        const notice = document.querySelector('.notice');
+      const noticeText = document.querySelector('.noticeText');
+      noticeText.textContent="Stack Overflow"
+      notice.style.visibility ="visible";
+      }
+    }
+    else{
+      const notice = document.querySelector('.notice');
+      const noticeText = document.querySelector('.noticeText');
+      noticeText.textContent="Enter A Stack First"
+      notice.style.visibility ="visible";
+    }
+  };
+
+  const popStack = () => {
+
+    if(stack.length > 0){
+      stack.splice(-1);
+      setStack([...stack]);
+    }
+    else{
+      const notice = document.querySelector('.notice');
+      const noticeText = document.querySelector('.noticeText');
+      notice.style.visibility ="visible";
+      noticeText.textContent="Stack Underflow"
+    }
+  };
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputs">
+        <input type="text" value={newTitle} onChange={e => setnewTitle(e.target.value)} placeholder="Enter An Element" />
+        <button onClick={handleChnageData}>Stack It</button>
+        <button onClick={popStack}>Pop It</button>
+      </div>
+
+      <div className="notice">
+        <p className="noticeText"></p>
+      </div>
+
+      <div className="stacks">
+          {
+            stack.map((item , index)=>{
+                return (
+                  <p>{item}</p>
+            )
+            })
+          }
+      </div>
     </div>
   );
 }
